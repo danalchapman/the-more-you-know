80a4cfe4ddb94204941fb844e66c8f438aef57ae
Dana Chapman <dana.leigh.dlc@gmail.com>
2023-02-10 15:04:44 -0600

    Create initial file structure

---
+++ src/Components/App/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>The More You Know</h1>
+    </div>
+  );
+}
+
+export default App;
