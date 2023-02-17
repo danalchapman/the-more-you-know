ee3b1953019039116cc900afe9f66cc493eb8a4b
Dana Chapman <dana.leigh.dlc@gmail.com>
2023-02-17 11:20:51 -0600

    Add behavioral questions to dataset

--- src/Data/Behavioral.tsx
+++ src/Data/Behavioral.tsx
@@ -1,1 +1,167 @@
-export {}
+export const bData = [
+    {
+        id: 'b1',
+        question: 'What made you switch from your prior career into programming?',
+        answer: ''
+    },
+    {
+        id: 'b2',
+        question: 'How do you familiarize yourself with a codebase you haven’t worked in before?',
+        answer: ''
+    },
+    {
+        id: 'b3',
+        question: 'Describe to me the steps you would take to build out a product.',
+        answer: ''
+    },
+    {
+        id: 'b4',
+        question: 'Tell me how you solve a problem.',
+        answer: ''
+    },
+    {
+        id: 'b5',
+        question: 'Describe your background. How did you get to where you are now? What was your previous career field. What did you like about it? What didn\'t you like about it? Why coding?',
+        answer: ''
+    },
+    {
+        id: 'b6',
+        question: 'Describe your time at Turing. What worked for you? What didn\'t work for you? What was the structure of the program?',
+        answer: ''
+    },
+    {
+        id: 'b7',
+        question: 'Describe your teamwork experience in a coding environment. How big of a team did you work with?',
+        answer: ''
+    },
+    {
+        id: 'b8',
+        question: 'A lot of this job will be full-stack. Your experience is largely __ (Back End, Front End). How do you feel like you\'d be able to contribute to the team with only that experience? How comfortable are you with getting uncomfortable with a new language?',
+        answer: ''
+    },
+    {
+        id: 'b9',
+        question: 'Describe your coding experience and why you wish to continue?',
+        answer: ''
+    },
+    {
+        id: 'b10',
+        question: 'What are you looking for in a company? Why would you want to work here?',
+        answer: ''
+    },
+    {
+        id: 'b11',
+        question: 'Describe an example of how you have handled a stressful situation.',
+        answer: ''
+    },
+    {
+        id: 'b12',
+        question: 'Have you ever had to switch gears in the middle of a project to get something fixed immediately? How did you react?',
+        answer: ''
+    },
+    {
+        id: 'b13',
+        question: 'What is your 100% ideal role for your first web development job?',
+        answer: ''
+    },
+    {
+        id: 'b14',
+        question: 'Tell me about a time you had a disagreement with a colleague and how you resolved it.',
+        answer: ''
+    },
+    {
+        id: 'b15',
+        question: 'What project of yours are you most proud of and why?',
+        answer: ''
+    },
+    {
+        id: 'b16',
+        question: 'Explain one of the most difficult code problems you\'ve had to solve, either on your own authored code, or in contributing to someone else\'s.',
+        answer: ''
+    },
+    {
+        id: 'b17',
+        question: 'Describe a time when you received feedback that you were surprised by. What did you do with that feedback?',
+        answer: ''
+    },
+    {
+        id: 'b18',
+        question: 'Describe a successful idea or project you worked on. What are some of the challenges you had to overcome? What made it a success?',
+        answer: ''
+    },
+    {
+        id: 'b19',
+        question: 'Describe a project that failed, a project you had to give up on or a time you worked really hard on a project and it just didn’t pan out as expected. What happened? How did you make the decisions that you made?',
+        answer: ''
+    },
+    {
+        id: 'b20',
+        question: 'What is your approach to working on a team?',
+        answer: ''
+    },
+    {
+        id: 'b21',
+        question: 'What is an area of growth for you?',
+        answer: ''
+    },
+    {
+        id: 'b22',
+        question: 'Describe your strengths. How have you seen these in practice as a developer?',
+        answer: ''
+    },
+    {
+        id: 'b23',
+        question: 'What is something that your hobbies or interests tell me about you that your resume cannot?',
+        answer: ''
+    },
+    {
+        id: 'b24',
+        question: 'Tell me about a time you failed. How did you deal with the situation?',
+        answer: ''
+    },
+    {
+        id: 'b25',
+        question: 'Give me an example of a time you were able to be creative with your work. What was exciting or difficult about it?',
+        answer: ''
+    },
+    {
+        id: 'b26',
+        question: 'Sometimes it\'s just not possible to get everything on your to-do list done. Tell me about a time your responsibilities got a little overwhelming. What did you do?',
+        answer: ''
+    },
+    {
+        id: 'b27',
+        question: 'Tell me about a time you set a goal for yourself. How did you go about ensuring that you would meet your objective?',
+        answer: ''
+    },
+    {
+        id: 'b28',
+        question: 'Tell me about a time when you had to rely on written communication to get your ideas across to your team.',
+        answer: ''
+    },
+    {
+        id: 'b29',
+        question: 'Tell me about a successful presentation you gave and why you think it was a hit.',
+        answer: ''
+    },
+    {
+        id: 'b30',
+        question: 'Describe a long-term project that you managed. How did you keep everything moving along in a timely manner?',
+        answer: ''
+    },
+    {
+        id: 'b31',
+        question: 'Describe a time when you saw some problem and took the initiative to correct it rather than waiting for someone else to do it.',
+        answer: ''
+    },
+    {
+        id: 'b32',
+        question: 'Tell me about a time when you worked under close supervision or extremely loose supervision. How did you handle that?',
+        answer: ''
+    },
+    {
+        id: 'b33',
+        question: 'Tell me about a time you needed to get information from someone who wasn’t very responsive. What did you do?',
+        answer: ''
+    },
+]
