b319f0ae6e5d339a111d03bba35665cf6e1bacbc
Dana Chapman <dana.leigh.dlc@gmail.com>
2023-02-17 11:52:34 -0600

    Connect App, QuestionsBox, and QuestionCard Components

--- src/Components/QuestionCard/QuestionCard.tsx
+++ src/Components/QuestionCard/QuestionCard.tsx
@@ -1,1 +1,18 @@
-export {}
+import { useState } from "react"
+import './QuestionCard.css'
+
+export const QuestionCard = () => {
+    // need to pass props from QuestionsBox
+    // ternary for the button 
+    // clicked ? question.answer : 'Click Me For The Answer'
+
+    const [clicked, setClicked] = useState(false)
+
+    return (
+        <article>
+            <h3>I am a Question Title</h3>
+            <p>I am a Question</p>
+            <button>Click Me For the Answer</button>
+        </article>
+    )
+}
--- src/Components/QuestionsBox/QuestionsBox.tsx
+++ src/Components/QuestionsBox/QuestionsBox.tsx
@@ -1,1 +1,17 @@
-export {}
+import { QuestionCard } from '../QuestionCard/QuestionCard'
+import './QuestionsBox.css'
+
+export const QuestionsBox = () => {
+    // need to map through questions, similar to trivia trials
+    // write functions for onClick for each button
+
+    return (
+        <section>
+            <h2>Choose Your Topic</h2>
+            <div>
+                <button>Behavioral Questions</button>
+                <button>Technical Questions (Front End)</button>
+            </div>
+        </section>
+    )
+}
